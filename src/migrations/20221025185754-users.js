79cff383dc0b86e09b94823f75e1e6704aac6651
Tassio-Med <tassiohfmed@gmail.como>
2022-10-25 16:34:17 -0300

    feat: migrations criadas

---
+++ src/migrations/20221025185754-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      displayName: {
+        allowNull: false,
+        field: 'display_name',
+        type: Sequelize.STRING(255),
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(255),
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING(255),
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING(255),
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
