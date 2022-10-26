247e969b4bbfd34c21b903fb6e658259f9a8ca98
Tassio-Med <tassiohfmed@gmail.como>
2022-10-26 17:04:46 -0300

    :sparkles: feat: model Category criada

---
+++ src/models/Category.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true },
+      name: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      underscored: true, 
+      tableName: 'categories'
+    },
+  );
+
+  return Category;
+}
